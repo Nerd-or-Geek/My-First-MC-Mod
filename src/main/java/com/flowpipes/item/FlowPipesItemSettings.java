8acc7f724dd1021bf254804cb849201a46db0f45
topplanecreator <michael6gledhill@gmail.com>
2026-01-12 10:16:35 -0500

    1.2.7

--- src/main/java/com/flowpipes/item/FlowPipesItemSettings.java
+++ src/main/java/com/flowpipes/item/FlowPipesItemSettings.java
@@ -1,12 +1,15 @@
 package com.flowpipes.item;
 
 import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
 
 /**
- * Custom Item.Settings that provides a default translation key without calling super
+ * Custom Item.Settings that provides default values for translation key and model ID
+ * without calling super, bypassing Minecraft's null checks that occur before registration
  */
 public class FlowPipesItemSettings extends Item.Settings {
 	private String translationKey = "item.placeholder";
+	private Identifier modelId = Identifier.of("flowpipes", "placeholder");
 
 	public FlowPipesItemSettings() {
 		super();
@@ -16,5 +19,11 @@ public class FlowPipesItemSettings extends Item.Settings {
 	public String getTranslationKey() {
 		// Return the translation key we set, bypassing super's null check
 		return translationKey;
+	}
+
+	@Override
+	public Identifier getModelId() {
+		// Return the model ID we set, bypassing super's null check
+		return modelId;
 	}
 }
